b1965818e08928effb8255bc64258038ae415a26
Johnathon Dirstein <johndirstein@gmail.com>
2017-03-15 15:48:15 -0400

    Initial commit

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log('works');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+	render() {
+		return (
+		<div>Heyyyyy!</div>
+		)
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
